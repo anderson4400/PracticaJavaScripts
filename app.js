e621ab6a3272e87bd1fc14c5870420ef76c94d93
Anderson Medina - Ogtic <anderson.medina@ogtic.gob.do>
2022-02-16 15:23:59 -0400

    segundo commit

--- app.js
+++ app.js
@@ -1,5 +1,50 @@
 //aqui esta la alerta.
-alert('Desde archivo externo 👀' );
+// alert('Desde archivo externo 👀' );
+// console.log('esto es entre comida dobles')
+// let a = "Anderson Medina";
 
-let a = "Anderson Medina";
-console.log(a);
+///Variables
+// let nombreUsuario = "Ignacion";
+// let edadUsuario = 33;
+// console.log(nombreUsuario);
+
+// let numeroUno = parseInt(prompt("Ingrese un numero"));
+// let numeroDos = parseInt(prompt("Ingrese un numero"));
+// let Resultado = numeroUno + numeroDos;
+
+// console.log(Resultado);
+
+// let opcionUsuario = prompt(`
+// Elija una opcion
+// 1: Libros
+// 2: Peliculas
+// 3: Juegos
+// `)
+// console.log(opcionUsuario);
+
+// switch(opcionUsuario){
+//     case '1':
+//         console.log('Principito');
+//     break;
+//     case '2':
+//         console.log('Matriz'); 
+//     break;  
+//     case '3':
+//         console.log('NFS');
+//     break;
+//     default:
+//     console.log('No ahi nada ');
+//     break
+// }
+
+// let numero = 5;
+// let ca = 0;
+// while(numero ===5){
+
+// ca++
+// alert("el valor ahora es:"+ca)
+//     if(ca===5){
+//         numero =4;
+//     }
+// }
+
